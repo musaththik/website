f3040b94a98f5822296d948bd5544ddf7d6b2455
Musaththik <musaththikmohamed@gmail.com>
2025-06-11 18:42:41 +0400

    added script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const button = document.getElementById("changeColorBtn");
+
+button.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomColor();
+});
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
